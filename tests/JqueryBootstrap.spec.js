6becd98637e917d1a21d98e46800e7caf55fe1ea
nikhilcogni <nikhilcogni@gmail.com>
2023-12-03 09:01:48 +0000

    Modified the website and locators

--- tests/JqueryBootstrap.spec.js
+++ tests/JqueryBootstrap.spec.js
@@ -1,13 +1,20 @@
 import {expect, test} from '@playwright/test';
 
 test('JqueryBootstrap Drop down', async ({page}) => {
-    await page.goto("https://www.jquery-az.com/boots/demo.php?ex=63.0_1");
+    await page.goto("http://seleniumpractise.blogspot.com/2016/08/bootstrap-dropdown-example-for-selenium.html");
 
     //click on the dropdown button
-    await page.locator(".multiselect.dropdown-toggle.btn.btn-default").click();
+    await page.locator("#menu1").click();
 
     //get the list of elements present in the dropdown
-    const options = await page.locator("ul.multiselect-container.dropdown-menu li a label input").count();
+    const options = await page.locator("ul.dropdown-menu li a").count();
     await console.log(options);
-    await expect(options).toEqual(5);
+    await expect(options).toEqual(4);
+
+    for (let i = 0; i < options; i++) {
+        let innertext_option = await page.locator("ul.dropdown-menu li a").textContent();
+        if (innertext_option === "HTML" || innertext_option === "Angular JS") {
+            await page.locator("ul.dropdown-menu li a").click();
+        }
+    }
 });
